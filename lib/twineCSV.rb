3e564feb668da96ce703edc58ce33d7ca3f671e1
Stefan Neidig <s.neidig@appcom-interactive.de>
2016-05-05 11:13:29 +0200

    Add methods for converting to csv and twine respectively

--- lib/twineCSV.rb
+++ lib/twineCSV.rb
@@ -1,5 +1,72 @@
 require "twineCSV/version"
+require 'rubygems'
 
 module TwineCSV
-  # Your code goes here...
+
+  def self.to_csv input, output
+    abort("The input file must not be nil") if input.nil? or output.nil?
+    content = File.open(input, 'r').read
+    lines = content.each_line.to_a.map { |line| line.gsub("\n", '').strip }
+    dictionary = {}
+    langs = []
+    current_key = ''
+    current_section = ''
+
+    lines.reject { |line| line.length == 0 }.each { |line|
+      if line.start_with? '[['
+        current_section = line[2..-3]
+        puts current_section
+        dictionary[current_section] = {}
+      elsif line.start_with? '['
+        current_key = line[1..-2]
+        dictionary[current_section][current_key] = {}
+      elsif current_key.length > 0
+        lang, value = line.split("=").map(&:strip)
+        langs << lang
+        dictionary[current_section][current_key][lang] = value
+      end
+    }
+
+    File.open(output, 'wb+') { |f|
+
+      f << 'section;key;' << langs.uniq.join(';') << "\n"
+
+      dictionary.each { |k, v|
+        v.each { |k2, v2|
+          vlangs = langs.uniq.map { |lang| v2[lang] }
+          f << "#{k};#{k2};#{vlangs.join(";")}" << "\n"
+        }
+      }
+    }
+
+  end
+
+  def self.to_twine input, output
+    abort("The input file must not be nil") if input.nil? or output.nil?
+
+    content = File.open(input, 'r').read
+    lines = content.each_line.to_a.map { |line| line.gsub("\n", '').strip }
+    current_section = ''
+    result = []
+    langs = lines[0].split(';')[2..-1]
+
+    lines[1..-1].each { |line|
+      values = line.split(";")
+      old_section = current_section
+      current_section = values.first
+
+      if current_section != old_section
+        result << "#{result.empty? ? '' : "\n"}[[#{current_section}]]"
+        result << "  [#{values[1]}]" << values[2..-1].map.with_index { |value,i| "    #{langs[i]} = #{value}"}
+      else
+        result << "\n  [#{values[1]}]" << values[2..-1].map.with_index { |value,i| "    #{langs[i]} = #{value}"}
+      end
+    }
+
+    File.open(output, 'wb+') { |f|
+      f << result.join("\n")
+    }
+
+  end
+
 end
